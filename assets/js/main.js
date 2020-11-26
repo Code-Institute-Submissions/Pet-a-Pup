350176648c3effdb024466a0db0558048547e099
MarcusAureliusLinden <gottemaja@gmail.com>
2020-11-26 22:56:16 +0000

    start game & popout functions amended, now working again

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+// HTML-declarations
+
+const pups = document.querySelectorAll(".pup");
+const holes = document.querySelectorAll(".hole");
+const scoreDisplay = document.getElementById("score");
+const startButton = document.getElementById("start");
+const magicDog = document.getElementById("pup3")
+
+//Select random hole
+
+let prevHole;
+
+function randomHole(holes) {
+    const holeId = Math.floor(Math.random() * holes.length);
+    const hole = holes[holeId];
+    if (hole === prevHole) {
+        return randomHole(holes);
+    }
+    prevHole = hole;
+    return hole;
+}
+
+// Make pup come out function
+
+let timeUp = false;
+
+function pupOut() {
+    if (timeUp) {
+        return;
+    }
+    const hole = randomHole(holes);
+    const time = Math.random() * 2000 + 500;
+    hole.classList.add("up");
+    timeUp = true;
+    setTimeout(function(){
+        hole.classList.remove("up");
+        //if (timeUp = false) {
+         //   pupOut();
+        //} 
+        timeUp = false;
+    }, time);
+}
+
+//Petting functions, to add score as mouse moves over, also magic dog function to add extra points
+
+let score = 0;
+
+function pet(e){
+    score++;
+    scoreDisplay.textContent = score;
+}
+pups.forEach(pup => pup.addEventListener("mousemove", pet));
+
+//pups.forEach(pup => pup.addEventListener("onmousemove", pet));
+
+/*function magicPet(e) {
+    score+100;
+    score.textContent = score;
+}
+magicDog.addEventListener("click", magicPet);
+function blackCat score-1000;
+*/
+
+function startGame() {
+    score = 0;
+    score.textContent = 0;
+    setInterval(function () {
+        /*setTimeout(function () {
+            alert("You petted x dogs");
+            timeUp = true;
+            pupOut();
+        }, Math.random() * 6000)*/
+        pupOut();
+    }, 1000);
+}
+    
+    
